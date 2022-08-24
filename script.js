47e159bc90fab9dc401411c23511ac8c8185a595
ares <kisaoglugurur@gmail.com>
2022-08-24 19:16:22 +0300

    added main files and images

---
+++ script.js
@@ -0,0 +1,10 @@
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navlist");
+
+menu.addEventListener("click", function(){
+    navbar.classList.toggle("active");
+});
+
+window.onscroll = () => {
+    navbar.classList.remove("active");
+}
